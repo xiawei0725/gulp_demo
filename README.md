970a98beff6b55dcd3dd933d3b9c37728d8f930a
xiawei <54879490@qq.com>
2021-02-04 14:20:26 +0800

    test

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 - 开发启动 `npm run dev`
 
 - 由于`gulp`对`polyfill` 有点问题，所以我这里支持两种构建打包，如果你的代码没有使用ES6+的高级API或者一些垫片，那么你可以直接`npm run build2`,如果你使用了`Promise`或者`Array.prototype.includes`等高级特性，那么你就运行`npm run build1`,当然`build1`我也保留了原汁原味的打包，你可以看看打包后的结果，但是不能用于浏览器环境。
+
+- `index.html`里面直接引入`dist`开头的静态资源,每次`build`会删除`dist`
---
+++ dist/js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("core-js/modules/es.object.to-string.js");
+
+require("core-js/modules/es.promise.js");
+
+require("core-js/modules/web.timers.js");
+
+var _require = require('./test.js'),
+    bar = _require.bar;
+
+bar();
+
+var foo = function foo(x) {
+  return x + 2;
+};
+
+var str = "hello gulp";
+console.log(str);
+var p1 = new Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve('hello');
+  }, 1000);
+});
+p1.then(function (res) {
+  console.log(res);
+});
